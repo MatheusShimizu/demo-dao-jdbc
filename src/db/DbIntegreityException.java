6a85793a76a3ae6c463a8f8ad0f4beae80a322ec
Matheus Shimizu <matheushimi@gmail.com>
2020-06-17 21:46:17 -0300

    criando projeto e repositório Git

---
+++ src/db/DbIntegreityException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbIntegreityException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbIntegreityException (String msg) {
+		super(msg);
+	}
+
+}
